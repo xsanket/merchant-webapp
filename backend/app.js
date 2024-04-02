0605ac8b66928a0c655b21e93b2818cf9907c587
xsanket <xsanketkamble232@gmail.com>
2024-04-03 00:12:12 +0530

    "bug fix"

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ import restLogin from './routes/restLoginRoute.js'
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
